dcaf8daa19cf1cf5b189f1f931e4c01849e4a921
Castasebas04 <castasebas04@gmail.com>
2026-05-23 23:24:59 -0500

    Improve comments in tiles.py
    Refactor comments for clarity and remove unnecessary text.

--- tiles.py
+++ tiles.py
@@ -17,19 +17,18 @@ def cut_tiles_fixed(image_path, output_dir, tile_size=512, overlap=0, bg_thresho
     stride = tile_size - overlap
     saved_count = 0
     
-    # CAMBIO 1: El rango va hasta el final (width y height completos)
+    
     for y in range(0, height, stride):
         for x in range(0, width, stride):
             
-            # CAMBIO 2: Definir coordenadas reales (sin salirse de la imagen)
+            # Definir coordenadas reales 
             x_end = min(x + tile_size, width)
             y_end = min(y + tile_size, height)
             
-            # Extraer el recorte (puede ser más pequeño que 512x512 en los bordes)
+            # Extraer el recorte 
             tile = img.crop((x, y, x_end, y_end))
             
-            # CAMBIO 3: Padding (Relleno) si el tile es pequeño
-            # Si el tile no es de 512x512, creamos un lienzo blanco y pegamos el tile
+            #  Padding (Relleno) si el tile es pequeño
             if tile.size != (tile_size, tile_size):
                 new_tile = Image.new("RGB", (tile_size, tile_size), (255, 255, 255)) # Fondo blanco
                 new_tile.paste(tile, (0, 0))
@@ -45,20 +44,19 @@ def cut_tiles_fixed(image_path, output_dir, tile_size=512, overlap=0, bg_thresho
                 if saved_count % 100 == 0:
                     print(f"Guardados {saved_count} tiles...", end='\r')
 
-    print(f"\n✅ Proceso completado. Tiles guardados: {saved_count}")
+    print(f"\n Proceso completado. Tiles guardados: {saved_count}")
 
 def is_mostly_background(tile, threshold=0.8):
-    # Optimización: Convertir a 'L' (Grayscale) directamente con PIL es más rápido que Numpy
+   
     gray = np.array(tile.convert('L'))
     
-    # 210 es un buen umbral para "beige/blanco"
     white_pixels = np.sum(gray > 140) 
     white_ratio = white_pixels / gray.size
     
     return white_ratio > threshold
 
 if __name__ == "__main__":
-    # Recuerda usar r"" para las rutas en Windows para evitar errores con \
+
     image_path = r"C:\Users\Sebastián\Documents\Uninorte\9no semestre\PF\PF repository\Imagenes\BR-007-TRICROMICO-25.tif"
     output_dir = r"C:\Users\Sebastián\Documents\Uninorte\9no semestre\PF\PF repository\tiles_img"
     
